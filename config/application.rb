6c14c952d587a383e9b3b553832638b62b01b88a
天野太郎 <amanotaro@amanotarounoMacBook-Air.local>
2024-11-07 00:17:43 +0900

    heroku環境下でのパスワードリセット機能

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,16 @@ module Myapp
 
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
+
+    # Add the following block to load production settings from production.yml
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'settings', 'production.yml')
+      if File.exists?(env_file)
+        YAML.load_file(env_file)[Rails.env].each do |key, value|
+          ENV[key.to_s] = value.to_s
+        end
+      end
+    end
   end
 end
+
