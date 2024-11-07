b2297a799c71e02cce225d1698af47eaee0183cb
天野太郎 <amanotaro@amanotarounoMacBook-Air.local>
2024-11-08 00:18:03 +0900

    sendgrid認証

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = Settings.default_url_options.to_h
+  config.action_mailer.default_url_options = { from: 'info@midorino-keijiban.com' }
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
