9b276f0dac73571016462be20c6bd6781ae5393b
天野太郎 <amanotaro@amanotarounoMacBook-Air.local>
2024-11-08 00:56:45 +0900

    送信エラー修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: 'infinite-coast-76610.herokuapp.com', protocol: 'https' }
+
+  Rails.application.routes.default_url_options[:host] = 'infinite-coast-76610.herokuapp.com'
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
