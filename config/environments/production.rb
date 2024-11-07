832f1d25ef6113d96cd80a48993eabe1ad366dec
天野太郎 <amanotaro@amanotarounoMacBook-Air.local>
2024-11-08 00:32:35 +0900

    Fix missing host in production email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { from: 'info@midorino-keijiban.com' }
+  config.action_mailer.default_url_options = { host: 'infinite-coast-76610.herokuapp.com', protocol: 'https' }
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
