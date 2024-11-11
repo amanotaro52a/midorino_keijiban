a492da933e5fa982c083ac7129cb31f08552d887
天野太郎 <amanotaro@amanotarounoMacBook-Air.local>
2024-11-11 11:39:31 +0900

    gmail仕様に変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,11 +103,11 @@ Rails.application.configure do
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.sendgrid.net',
+    address: 'smtp.gmail.com',
     port: 587,
-    user_name: 'apikey', # SendGridの環境変数
-    password: ENV['SENDGRID_API_KEY'], # SendGridの環境変数
-    domain: 'herokuapp.com',
+    user_name:            ENV['SMTP_USER_NAME'],
+    password:             ENV['SMTP_PASSWORD'],
+    domain: 'infinite-coast-76610-6cf707f3e38e.herokuapp.com',
     authentication: 'plain',
     enable_starttls_auto: true
   }
