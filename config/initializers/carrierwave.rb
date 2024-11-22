0481ea09e5f4017024f644cce6bab455eb745da5
天野太郎 <amanotaro@amanotarounoMacBook-Air.local>
2024-11-22 14:04:25 +0900

    プリコンパイルエラー解消

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,10 @@
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.production?
-    config.fog_provider = 'fog/google'
-    config.fog_credentials = {
-      provider: 'Google',
-      google_project: ENV['GOOGLE_CLOUD_PROJECT'],
-      google_json_key_string: ENV['GOOGLE_CLOUD_KEYFILE_JSON']
-    }
-    config.fog_directory  = ENV['GOOGLE_CLOUD_STORAGE_BUCKET']
-    config.storage :fog
-  else
-    config.storage :file
-  end
+  config.fog_credentials = {
+    provider: 'Google',
+    google_project: ENV['GOOGLE_CLOUD_PROJECT'],
+    google_json_key_string: ENV['GOOGLE_CLOUD_KEYFILE_JSON']
+  }
+  config.fog_directory  = ENV['GOOGLE_CLOUD_STORAGE_BUCKET']
 end
