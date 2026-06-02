8cf9ae40c0686126baac6358f9948ba8e9f123a9
天野太郎 <amanotaro@amanotarounoMacBook-Air.local>
2026-06-02 16:03:42 +0900

    ルーティングファイルのリファクタリング

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => "/admin", as: "rails_admin"
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -15,8 +14,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "static_pages#top"
   resources :users, only: %i[new create]
-  resources :diaries, only: %i[index new create show edit destroy update] do
-  end
+  resources :diaries, only: %i[index show new create edit destroy update] 
   resource :profile, only: %i[show edit update]
   resources :password_resets, only: %i[new create edit update]
   resources :contacts, only: %i[new create]
@@ -24,7 +22,6 @@ Rails.application.routes.draw do
   get "terms_of_service", to: "informations#terms_of_service", as: :informations_terms_of_service
   get "privacy_policy", to: "informations#privacy_policy", as: :informations_privacy_policy
   get "how_to_used", to: "informations#how_to_used", as: :informations_how_to_used
-  get "contacts", to: "contacts#new", as: :contacts_new
 
   get "login", to: "user_sessions#new"
   post "login", to: "user_sessions#create"
