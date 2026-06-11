c9e0310dcc620b129f9ab443b081b9ff4d3bad59
天野太郎 <amanotaro@amanotarounoMacBook-Air.local>
2026-06-11 23:50:06 +0900

    投稿一覧画面の引っ越し

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
-      redirect_to posts_path, success: t("defaults.flash_message.created", item: Post.model_name.human)
+      redirect_to root_path, success: t("defaults.flash_message.created", item: Post.model_name.human)
     else
       flash.now[:danger] = t("defaults.flash_message.not_created", item: Post.model_name.human)
       render :new, status: :unprocessable_entity
